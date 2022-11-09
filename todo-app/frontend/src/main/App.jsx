caebf880bd8472fdd4493938b0a4e5ff4cc68846
ArthurBHS <arthur.shekinarfoxy@gmail.com>
2022-11-09 10:54:29 -0300

    configuração de rotas

--- todo-app/frontend/src/main/App.jsx
+++ todo-app/frontend/src/main/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import Todo from '../todo/Todo'
-import About from '../about/About'
+import Routes from './Routes'
 import Menu from '../template/Menu'
 
 require('bootstrap/dist/css/bootstrap.min.css')
@@ -9,9 +8,8 @@ require('font-awesome/css/font-awesome.min.css')
 
 const App = props => (
     <div className="container">
-        <Menu />
-        <Todo />
-        <About />
+        <Menu/>
+       <Routes/>
     </div>
 )
 export default App
